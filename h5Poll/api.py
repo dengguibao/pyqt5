a575f5b534b1df0333301c328d1ae3ebe83fe2ec
dengguibao <tydgb@outlook.com>
2020-09-20 13:21:01 +0800

    update  h5poll

--- h5Poll/api.py
+++ h5Poll/api.py
@@ -21,7 +21,7 @@ class Poll:
         'Origin': domain,
         'Sec-Fetch-Mode': 'cors',
         'Sec-Fetch-Site': 'same-origin',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0',
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
         'X-Requested-With': 'XMLHttpRequest'
     }
     s = None
@@ -34,7 +34,7 @@ class Poll:
             s.proxies = self.proxies
         self.s = s
 
-    def validate_url(self, url):
+    def build_post_data(self, url):
         args = self.get_url_args(url)
         data = {
             '2020011919401334501053001': {
@@ -78,16 +78,41 @@ class Poll:
             return data[args['questionnaireId']], None
         else:
             return '%s 无效链接,未找到token或者问卷ID' % url, True
+    
+    def cookie_and_jumpurl(self, url):
+        r = self.s.get(url, verify=False, allow_redirects=False)
+
+        if (r.status_code == 200 or r.status_code == 302) and 'Location' in r.headers:
+            self.req_headers['Cookie'] = r.headers['Set-Cookie']
+            self.s.headers.update(self.req_headers)
+            return r.headers['Location']
+        else:
+            return None
+    
+    # def verify_link_is_invalid(self, url):
+    #     r = self.s.get(url, verify=False)
+
+    #     if r.status_code == 200:
+    #         if '重复提交' in r.text:
+    #             return '%s 链接已提交,请勿重复提交' % url
+    #         if '请求超时' in r.text:
+    #             return '%s 链接已失效超时' % url
+    #     else:
+    #         return None
 
     def submit_poll(self, url):
-        r = self.s.get(url, verify=False)
-        if '重复提交' in r.text:
-            return '%s 链接已提交,请勿重复提交' % url
-        if '请求超时' in r.text:
-            return '%s 链接已失效超时' % url
+        # jumpurl = self.cookie_and_jumpurl(url)       
 
-        jump302_url = r.url
-        d, err = self.validate_url(jump302_url)
+        # if not jumpurl:
+        #     return '%s 链接提交' % url
+        
+        # r = self.verify_link_is_invalid(jumpurl)
+        # if r:
+        #     return r
+
+        jumpurl = 'https://eva.customer.10086.cn/survey/QnrTomatoOrange.html?token=cPhrRI769943&&questionnaireId=2020081910351107201051001&&tokenflag=926ca3b1'
+        
+        d, err = self.build_post_data(jumpurl)
         if err:
             return d
 
@@ -107,7 +132,7 @@ class Poll:
 
 
         # print(post_data)
-        self.req_headers['Referer'] = jump302_url
+        self.req_headers['Referer'] = jumpurl
         # requests lib default header
         r = self.post_req(post_data)
 
@@ -120,6 +145,7 @@ class Poll:
             return 'msg:failed, status_code:%s, %s, return_text:%s' % (
                 r.status_code, tmp_str, r.text
             )
+        del self.req_headers['Cookie']
 
     @staticmethod
     def get_url_args(url):
@@ -133,7 +159,7 @@ class Poll:
     def post_req(self, post_data):
         self.s.headers.update(self.req_headers)
         url = '%s/surveyH5Response/dynamicQuestion' % self.domain
-        # url = 'http://127.0.0.1:8080/test'
+        url = 'http://127.0.0.1:8000/test'
 
         return self.s.post(
             url,
